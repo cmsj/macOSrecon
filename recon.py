9dd012b70e84286458c893e79361bf76c5f65ab7
Chris Jones <cmsj@tenshu.net>
2017-02-07 20:06:29 +0000

    Fix typo

--- recon.py
+++ recon.py
@@ -9,7 +9,7 @@ OUTPUT = "data/"
 def main():
     """Main entry point"""
     boardIDs.dumpX86(OUTPUT + "boardIDs-x86.txt")
-    boardIDs.dumpACPI_SMC(OUTPUT + "boardIds-ACPI_SMC")
+    boardIDs.dumpACPI_SMC(OUTPUT + "boardIDs-ACPI_SMC.txt")
 
 
 if __name__ == "__main__":
