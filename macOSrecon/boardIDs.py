a340573a3dd9b97f066a8313443597769629ad0c
Chris Jones <cmsj@tenshu.net>
2017-08-06 16:58:46 +0200

    Add boardID->modelID lookups, thanks to piker alpha: https://github.com/Piker-Alpha/HandyScripts

--- macOSrecon/boardIDs.py
+++ macOSrecon/boardIDs.py
@@ -1,5 +1,18 @@
 """Functions relating to board-id gathering"""
 import os
+import objc
+
+
+class attrdict(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+
+
+ServerInformation = attrdict()
+bundle_path = '/System/Library/PrivateFrameworks/ServerInformation.framework'
+ServerInformation_bundle = objc.loadBundle('ServerInformation',
+                                           ServerInformation,
+                                           bundle_path=bundle_path)
 
 
 def gatherPlistFiles(filePath):
@@ -21,9 +34,22 @@ def gatherX86():
     return gatherPlistFiles(plistPath)
 
 
+def mapBoardsToModels(boardList):
+    """Map board-id values to model names"""
+    boardModelMap = []
+    for boardID in boardList:
+        for modelID in ServerInformation.ServerInformationComputerModelInfo.modelPropertiesForBoardIDs_([boardID]):
+            if boardID not in modelID:
+                boardModelMap.append("%s - %s" % (boardID, modelID))
+            else:
+                boardModelMap.append("%s - Unknown" % boardID)
+
+    return boardModelMap
+
+
 def dumpX86(filePath):
     """Gather and dump board-id values to a file"""
-    dumpListToFile(gatherX86(), filePath)
+    dumpListToFile(mapBoardsToModels(gatherX86()), filePath)
 
 
 def gatherACPI_SMC():
